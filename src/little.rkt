27c77f24528c771670c46a81379c2727f0ce940e
Xie Yuheng <xyheme@gmail.com>
2020-02-24 15:26:04 +0800

    up to `lookup-in-entry`

--- src/little.rkt
+++ src/little.rkt
@@ -12,9 +12,40 @@
 
 (define new-entry build)
 
-(assert (eq? (first (build 'a 'b)) 'a))
-(assert (eq? (second (build 'a 'b)) 'b))
+(assert (eq? (first (build 'a 'b))
+             'a))
 
-;; (define lookup-in-entry
-;;   (lambda (name entry)
-;;     ))
+(assert (eq? (second (build 'a 'b))
+             'b))
+
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help
+     name
+     (first entry)
+     (second entry)
+     entry-f)))
+
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? name (car names)) (car values))
+      (else (lookup-in-entry-help
+             name
+             (cdr names)
+             (cdr values)
+             entry-f)))))
+
+(define entry-1
+  (new-entry
+   '(appetizer entree beverage)
+   '(food tastes good)))
+
+(define identity (lambda (x) x))
+
+(assert (eq? (lookup-in-entry 'entree entry-1 identity)
+             'tastes))
+
+(assert (eq? (lookup-in-entry 'dessert entry-1 identity)
+             (identity 'dessert)))
